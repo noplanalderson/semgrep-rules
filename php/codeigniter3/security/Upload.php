b1c1bdef52c4afaeadac9039b4110fb512dfe460
Muhammad Ridwan Na'im <noplanalderson@gmail.com>
2025-04-23 17:46:27 +0700

    add: Vulnerable upload rules

---
+++ php/codeigniter3/security/Upload.php
@@ -0,0 +1,57 @@
+<?php
+
+class Upload extends CI_Controller {
+
+        public function __construct()
+        {
+                parent::__construct();
+                $this->load->helper(array('form', 'url'));
+        }
+
+        public function no_extension_validation()
+        {
+            $config['upload_path']          = './uploads/';
+            $config['max_size']             = 100;
+            $config['max_width']            = 1024;
+            $config['max_height']           = 768;
+
+            $this->load->library('upload', $config);
+
+            if ( ! $this->upload->do_upload('userfile'))
+            {
+                    $error = array('error' => $this->upload->display_errors());
+
+                    $this->load->view('upload_form', $error);
+            }
+            else
+            {
+                    $data = array('upload_data' => $this->upload->data());
+
+                    $this->load->view('upload_success', $data);
+            }
+        }
+
+        public function no_maxsize()
+        {
+            $config['upload_path']          = './uploads/';
+            $config['max_size']             = 100;
+            $config['max_width']            = 1024;
+            $config['max_height']           = 768;
+
+            $this->load->library('upload', $config);
+
+            if ( ! $this->upload->do_upload('userfile'))
+            {
+                    $error = array('error' => $this->upload->display_errors());
+
+                    $this->load->view('upload_form', $error);
+            }
+            else
+            {
+                    $data = array('upload_data' => $this->upload->data());
+
+                    $this->load->view('upload_success', $data);
+            }
+        }
+}
+?>
