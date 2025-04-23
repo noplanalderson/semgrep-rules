99af13f9c7a25e5b8dde1e1ab1c1e8c7c32ceab9
Muhammad Ridwan Na'im <noplanalderson@gmail.com>
2025-04-23 16:11:14 +0700

    Add another vulnerable method sample

--- php/codeigniter3/security/Sqli_model.php
+++ php/codeigniter3/security/Sqli_model.php
@@ -1,10 +1,25 @@
 <?php
 class Sqli_model extends CI_Controller {
-    public function vulnerable() {
+
+    // ruleid: codeigniter3-sql-injection
+    public function vuln_select() {
         $id = $_GET['id'];
         $query = $this->db->query("SELECT * FROM users WHERE id = $id");
     }
 
+    // ruleid: codeigniter3-sql-injection
+    public function vuln_delete() {
+        $id = $_POST['id'];
+        $query = $this->db->query("DELETE FROM users WHERE id = $id");
+    }
+
+    // ruleid: codeigniter3-sql-injection
+    public function vuln_insert() {
+        $query = "INSERT INTO users (name, email) VALUES ('" . $_POST['name'] . "', '" . $_POST['email'] . "')";
+        $query = $this->db->query($query);
+    }
+
+    // This code is safe
     public function safe() {
         $id = $_GET['id'];
         $query = $this->db->query("SELECT * FROM users WHERE id = ?", array($id));
