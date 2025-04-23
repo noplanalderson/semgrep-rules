86a4d84d277bced2b969e1dc9e523254ed19480d
Muhammad Ridwan Na'im <noplanalderson@gmail.com>
2025-04-23 16:48:37 +0700

    edit: Enhance rule detection for patter-source from input class

--- php/codeigniter3/security/Sqli_model.php
+++ php/codeigniter3/security/Sqli_model.php
@@ -8,6 +8,12 @@ class Sqli_model extends CI_Controller {
     }
 
     // ruleid: codeigniter3-sql-injection
+    public function vuln_select_with_input() {
+        $id = $this->input->get('id');
+        $query = $this->db->query("SELECT * FROM users WHERE id = $id");
+    }
+
+    // ruleid: codeigniter3-sql-injection
     public function vuln_delete() {
         $id = $_POST['id'];
         $query = $this->db->query("DELETE FROM users WHERE id = $id");
