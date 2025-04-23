bdbabd70e66e174238e734e44de8011b13111b72
Muhammad Ridwan Na'im <noplanalderson@gmail.com>
2025-04-23 17:57:29 +0700

    edit: Add new sample

--- php/codeigniter3/security/Upload.php
+++ php/codeigniter3/security/Upload.php
@@ -34,7 +34,32 @@ class Upload extends CI_Controller {
         public function no_maxsize()
         {
             $config['upload_path']          = './uploads/';
+            $config['allowed_types']        = 'gif|jpg|png';
+            $config['max_width']            = 1024;
+            $config['max_height']           = 768;
+
+            $this->load->library('upload', $config);
+
+            if ( ! $this->upload->do_upload('userfile'))
+            {
+                    $error = array('error' => $this->upload->display_errors());
+
+                    $this->load->view('upload_form', $error);
+            }
+            else
+            {
+                    $data = array('upload_data' => $this->upload->data());
+
+                    $this->load->view('upload_success', $data);
+            }
+        }
+
+        public function secure_upload()
+        {
+            $config['upload_path']          = './uploads/';
             $config['max_size']             = 100;
+            $config['allowed_types']        = 'gif|jpg|png';
+            $config['detect_mime']          = TRUE;
             $config['max_width']            = 1024;
             $config['max_height']           = 768;
 
